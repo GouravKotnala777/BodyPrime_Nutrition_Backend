a51b479ad6a4fc6eaac10698aba9a701cf674b28
gouravkotnala <gouravkotnala777@gmail.com>
2025-08-28 21:30:46 +0530

    chore : add dotenv, use urlencoded

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,14 @@
 import express from "express";
 import userRouter from "./routes/user.router.js";
+import dotenv from "dotenv";
+import cors from "cors";
 
+dotenv.config();
 
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
 
 
 export default app;
--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,11 @@
 import exppress from "express";
 import app from "./app.js";
+import connectDatabase from "./config/db.js";
 
-const PORT = 8000;
-
-
-app.use(exppress.urlencoded({extended:true}));
+const PORT = process.env.PORT || 8000;
 
 
-app.listen(PORT, () => console.log("Listening...."))
+app.listen(PORT, () => console.log("Listening...."));
+
+
+
