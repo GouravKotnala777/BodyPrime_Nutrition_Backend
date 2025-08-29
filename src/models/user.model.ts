2cf2a927a4748e59adaa0a54d7a2a7bf414668b4
gouravkotnala <gouravkotnala777@gmail.com>
2025-08-29 09:03:30 +0530

    feat(user.model.ts) : add database model for user

---
+++ src/models/user.model.ts
@@ -0,0 +1,39 @@
+import mongoose, { Model } from "mongoose";
+
+interface UserTypes{
+    name:string;
+    email:string;
+    password:string;
+    mobile:string;
+    gender:"male"|"female"|"other";
+    confirmPassword:string;
+}
+
+const userSchema = new mongoose.Schema<UserTypes>({
+    name:{
+        type:String,
+        required:true
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true
+    },
+    password:{
+        type:String,
+        required:true
+    },
+    mobile:{
+        type:String,
+        required:true
+    },
+    gender:{
+        type:String,
+        enum:["male", "female", "other"],
+        required:true
+    },
+}, {timestamps:true});
+
+const userModel:Model<UserTypes> = mongoose.models.User || mongoose.model<UserTypes>("User", userSchema);
+
+export default userModel;
