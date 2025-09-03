d0e38d5f88a27b4d6bc953247930017ee91764c4
gouravkotnala <gouravkotnala777@gmail.com>
2025-09-03 22:00:14 +0530

    feat(app.ts) : add cors

--- src/app.ts
+++ src/app.ts
@@ -13,6 +13,11 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
+app.use(cors({
+    origin:["http://localhost:5173"],
+    methods:["GET", "POST", "PUT", "DELETE"],
+    credentials:true
+}))
 
 app.use("/user", userRouter);
 app.use("/product", productRouter);
