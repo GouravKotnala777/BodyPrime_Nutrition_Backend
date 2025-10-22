ad3455ee2917169027f81f05f6770c288e9c7d3c
gouravkotnala <gouravkotnala777@gmail.com>
2025-10-22 12:53:23 +0530

    refactor : remove isUserAuthenticated middleware from single_product route

--- src/routes/product.router.ts
+++ src/routes/product.router.ts
@@ -7,7 +7,7 @@ const productRouter = express.Router();
 productRouter.route("/create_product").post(isUserAuthenticated, isUserAdmin, createProduct);
 productRouter.route("/search_products").get(searchProducts);
 productRouter.route("/get_products").get(getProducts);
-productRouter.route("/single_product/:productID").get(isUserAuthenticated, getSingleProduct);
+productRouter.route("/single_product/:productID").get(getSingleProduct);
 productRouter.route("/add_image").post(isUserAuthenticated, isUserAdmin, upload.array("images", 4), addImages);
 productRouter.route("/update_product").put(isUserAuthenticated, isUserAdmin, updateProduct);
 productRouter.route("/delete_product").delete(isUserAuthenticated, isUserAdmin, deleteProduct);
